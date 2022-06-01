5fb12da6e9372d682d0234d577e8e7e7e4269367
liuridong <liuridong@flashhold.com>
2022-06-01 18:57:52 +0800

    add config

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
+use nginx_log_read::Setting;
+use std::fs;
 use std::{thread, time};
 
 mod nginx_log_read;
 
 fn main() {
+    // 读取配置 /etc/horus/nginx_log_monitor.conf
+    let horus_setting = fs::read_to_string("/etc/horus/nginx_log_monitor.conf");
+    let mut horus_config = Setting {
+        nginx_log_path: String::from("/var/log/nginx/access.log"),
+        app_name: String::from("nginx"),
+        access_party: String::from("nginx"),
+        horus_host: String::from("127.0.0.1:80"),
+    };
+    if let Ok(setting) = horus_setting {
+        // 读取用户的配置
+        let user_config: Setting = serde_json::from_str(&setting).unwrap();
+        horus_config.nginx_log_path = user_config.nginx_log_path;
+        horus_config.app_name = user_config.app_name;
+        horus_config.access_party = user_config.access_party;
+        horus_config.horus_host = user_config.horus_host;
+    }
     // 10 秒
     let ten_seconds = time::Duration::from_secs(10);
     loop {
         println!("线程沉睡 10 秒！");
         thread::sleep(ten_seconds);
         println!("沉睡完成！");
-        nginx_log_read::read_log(&String::from(
-            "/home/archliu/workspace/arch/rust_proj/horus_log_col/access.log",
-        ));
+        nginx_log_read::read_log(&horus_config);
     }
 }
--- src/nginx_log_read.rs
+++ src/nginx_log_read.rs
@@ -1,5 +1,5 @@
 use reqwest::blocking::Client;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
@@ -7,17 +7,28 @@ use std::io::BufReader;
 use std::io::SeekFrom;
 use std::net::UdpSocket;
 
-#[derive(Debug, Serialize)]
-struct NginxLogRequest {
-    metricsCode: String,
-    metricsColumns: Vec<String>,
-    metricsColumnsValue: Vec<Vec<String>>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Setting {
+    pub nginx_log_path: String, // nginx 日志文件路径,默认为 /var/log/nginx/access.log
+    pub app_name: String,
+    pub access_party: String,
+    pub horus_host: String,
 }
 
-pub fn read_log(file_name: &String) {
+#[derive(Debug, Serialize)]
+struct NginxLogRequest {
+    #[serde(rename = "metricsCode")]
+    metrics_code: String,
+    #[serde(rename = "metricsColumns")]
+    metrics_columns: Vec<String>,
+    #[serde(rename = "metricsColumnsValue")]
+    metrics_columns_value: Vec<Vec<String>>,
+}
+
+pub fn read_log(setting: &Setting) {
     // 读取上一次文件读取的位置
-    println!("file name is :{} ", file_name);
-    let file = File::open(file_name).unwrap();
+    println!("file name is :{} ", setting.nginx_log_path);
+    let file = File::open(&setting.nginx_log_path).unwrap();
     let mut fin = BufReader::new(file);
     let mut read_cur = String::from("0");
     let read_res = fs::read_to_string("./read_index");
@@ -25,18 +36,18 @@ pub fn read_log(file_name: &String) {
         println!("{:?}", file);
         read_cur = file;
     }
-    println!("读取到的游标： {}", read_cur);
+    // println!("读取到的游标： {}", read_cur);
     fin.seek(SeekFrom::Current(read_cur.parse::<i64>().unwrap()))
         .expect("文件指针移动失败!");
     let mut read_len = 1;
-    let mut read_end_pos = 0;
+    let mut read_end_pos = read_cur.parse::<u64>().unwrap();
     let mut nginx_logs: Vec<Vec<String>> = Vec::new();
     while read_len != 0 {
         let mut line = String::new();
-        let start_pos = fin.stream_position();
-        if let Result::Ok(pos) = start_pos {
-            println!("start 指针： {}", pos);
-        }
+        // let start_pos = fin.stream_position();
+        // if let Result::Ok(pos) = start_pos {
+        //     println!("start 指针： {}", pos);
+        // }
         read_len = fin.read_line(&mut line).expect("read line error");
         // $msec]-[$remote_addr]-[$http_x_forwarded_for]-[$request_uri]-[$status]-[$http_user_agent]-[$request_time
         // print!("line: {}", line);
@@ -57,35 +68,41 @@ pub fn read_log(file_name: &String) {
         let end_pos = fin.stream_position();
         if let Result::Ok(pos) = end_pos {
             read_end_pos = pos;
-            println!("end 指针： {}", pos);
+            // println!("end 指针： {}", pos);
         }
     }
-    let nginx_log_request = NginxLogRequest {
-        metricsCode: String::from("nginx_log_monitor"),
-        metricsColumns: vec![
-            String::from("log_time"),
-            String::from("remote_addr"),
-            String::from("http_x_forwarded_for"),
-            String::from("request"),
-            String::from("status"),
-            String::from("http_user_agent"),
-            String::from("request_time"),
-        ],
-        metricsColumnsValue: nginx_logs,
-    };
-    let local_ip = get_local_ip().unwrap().replace(".", "_");
-    let url = String::from(
-        "http://172.31.239.66/api/horus/collection/metricsCol?appName=nginx&accessParty=nginx&instanceId=",
-    )
-    .to_string()
-        + &local_ip;
-
-    // 发送 horus 请求
-    let client = Client::new();
-    let resp = client.put(url).json(&vec![nginx_log_request]).send();
-    match resp {
-        Ok(res) => parse_resp(res),
-        Err(err) => println!("请求异常！ {:?}", err),
+    if nginx_logs.len() > 0 {
+        let nginx_log_request = NginxLogRequest {
+            metrics_code: String::from("nginx_log_monitor"),
+            metrics_columns: vec![
+                String::from("log_time"),
+                String::from("remote_addr"),
+                String::from("http_x_forwarded_for"),
+                String::from("request"),
+                String::from("status"),
+                String::from("http_user_agent"),
+                String::from("request_time"),
+            ],
+            metrics_columns_value: nginx_logs,
+        };
+        let local_ip = get_local_ip().unwrap().replace(".", "_");
+        let url = String::from("http://").to_string()
+            + &setting.horus_host
+            + "/api/horus/collection/metricsCol?appName="
+            + &setting.app_name
+            + "&accessParty="
+            + &setting.access_party
+            + "&instanceId="
+            + &local_ip;
+        // 发送 horus 请求
+        let client = Client::new();
+        let resp = client.put(url).json(&vec![nginx_log_request]).send();
+        match resp {
+            Ok(res) => parse_resp(res),
+            Err(err) => println!("请求异常！ {:?}", err),
+        }
+    } else {
+        println!("无新增日志！");
     }
     // 保存文件读取的位置
     let mut cur_file = std::fs::File::create("./read_index").expect("create failed");
